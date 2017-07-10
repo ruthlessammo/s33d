88c9d30023df9d21d0f4c91c8a7d2a455658052e
William Hilton <williamjchilton@gmail.com>
2017-07-10 12:56:17 +0100

    built sever

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// mongoose.plugin(require('./lib/globalToJSON'));
+mongoose.Promise = require('bluebird');
+const routes = require('./config/routes');
+const errorHandler = require('./lib/errorHandler');
+const customResponses = require('./lib/customResponses');
+
+const { port, env, dbURI } = require('./config/environment');
+const app = express();
+
+
+
+app.use(morgan('dev'));
+
+mongoose.connect(dbURI);
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(customResponses);
+app.use('/api', routes);
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Express is listening on port ${port}`));
+
+
+module.exports = app; // Exports the app to import in when we run our tests
