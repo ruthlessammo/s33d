e01699cc614b7e8d2b788f4695756b58643d6a47
William Hilton <williamjchilton@gmail.com>
2017-07-19 18:43:00 +0100

    updated dummy text first draft

--- client/src/components/Story.js
+++ client/src/components/Story.js
@@ -6,35 +6,78 @@ class Story extends Component {
       <div>
         <h1>Story</h1>
 
-        <h2>Aim: Lorem ipsum dolor sit amet, consectetur adipisicing.</h2>
+        <h2>Aim: Re-associating the supply chain</h2>
 
-        <div><h3>Why:</h3> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <div>
+          <h3>Why?</h3>
+          <p>
+            "It is now highly visible to take care of everybody on Earth at a higher standard of living than any other have ever known. It no longer has to be you or me. Selfishness is unnecessary. War is obsolete. It is a matter of converting high technology from weaponary to livingry." <em>Buckminster Fuller</em>
+          </p>
         </div>
 
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatexercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <div>
+          <p>
+            The legacy trade paradigm and subsequently the way the worlds value systems have been calibrated no longer serve the full potential of the the planet and its diverse range of inhabitants. Within the new trade paradigm of creative programmable capital we are able to fulfil a long term human vision of amplifying and preserving the value of the earths natural stores.
+          </p>
         </div>
 
         <div>
           <h3>Solution:</h3>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div><br/><br/>
+          <p>
+            Distribute a <strong><em>co-operative community driven exchange platform.</em></strong> By leveraging past, present, and future blockchain innovations to subsidies:
+          </p>
+          <ul>
+            <li>Spherical supply-chain awareness</li>
+            <li>The distribution of community educational incentives.</li>
+          </ul>
+        </div>
+        <br/><br/>
 
         <div>
           <h3>How:</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
           <ol>
-            <li>exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</li>
-            <li>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+            <li><p>Launch a crowdsourced <strong><em>cyrpto-assets</em></strong> knowledge network.</p>
+              <ul>
+                <li>Sowing a pioneering <strong><em>community-consensus</em></strong>experiment.</li>
+                <li>Growing a collectively managed pool of<strong><em>community capital.</em></strong></li>
+                <li>Germinating a unique <strong><em>community investment index.</em></strong></li>
+              </ul>
+            </li>
+            <li>
+              <p>Clear ROADMAP to the flowering of a more advanced community exchange platform ontology around:</p>
+              <ul>
+                <li>
+                  <p>The knowledge and trade of:</p>
+                  <ul>
+                    <li><strong>Seeds</strong> (and subsequently related products)</li>
+                    <li><strong>Sustainable services</strong> (beginning with energy production provenance)</li>
+                  </ul>
+                </li>
+                <li>
+                  <p>Incentivised P2P network scaling and innovation across both the seeds and service trade webs of:</p>
+                  <ul>
+                    <li><strong>Sustainable subsidies</strong></li>
+                    <li><strong>Provenance trails</strong></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
           </ol>
-
         </div><br/><br/>
 
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa.
-        </div>
-        <div>
-          <h3>Result:</h3> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa.
+          <h3>Result:</h3>
+          <p>S33D growing awareness and innovation cooperation platform anchored around the consumption, curation and creation of the pillars of modern human sustinence.</p>
+          <ul>
+            <li>Capital, Fuel, Food, Shelter, and Data use.</li>
+          </ul>
+          <p>Focused community architechural mandate to incentivise:</p>
+          <ul>
+            <li>The collation of transparent P2P <em>provenance webs</em></li>
+            <li><em>Extracting a unifying value</em> from the curation of a crypto-assets in a community coherence and diversity index.</li>
+            <li>The distribution of community co-education while fostering knowledge diversity.</li>
+            <li>The preservation of bio-diversity through recalibration of global capital stores.</li>
+          </ul>
         </div>
 
       </div>
