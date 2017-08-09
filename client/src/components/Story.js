ea16c43a41b2e5a0ba3d03c97506e70c5a25660f
William Hilton <williamjchilton@gmail.com>
2017-08-09 14:00:41 +0100

    added container using columns

--- client/src/components/Story.js
+++ client/src/components/Story.js
@@ -9,33 +9,39 @@ import ScrollableAnchor from 'react-scrollable-anchor'
 class Story extends Component {
   render() {
     return (
-      <div>
+      <Grid col={12}>
 
-        <Grid col={12}>
-          <ScrollableAnchor id={'why'}>
-            <Why />
-          </ScrollableAnchor>
+        <Grid col={2}></Grid>
+
+        <Grid col={8}>
+          <Grid col={12}>
+            <ScrollableAnchor id={'why'}>
+              <Why />
+            </ScrollableAnchor>
+          </Grid>
+
+          <Grid col={12}>
+            <ScrollableAnchor id={'solution'}>
+              <Solution />
+            </ScrollableAnchor>
+          </Grid>
+
+          <Grid col={12}>
+            <ScrollableAnchor id={'how'}>
+              <How />
+            </ScrollableAnchor>
+          </Grid>
+
+          <Grid col={12}>
+            <ScrollableAnchor id={'result'}>
+              <Result />
+            </ScrollableAnchor>
+          </Grid>
         </Grid>
+        <Grid col={2}></Grid>
 
-        <Grid col={12}>
-          <ScrollableAnchor id={'solution'}>
-            <Solution />
-          </ScrollableAnchor>
-        </Grid>
 
-        <Grid col={12}>
-          <ScrollableAnchor id={'how'}>
-            <How />
-          </ScrollableAnchor>
-        </Grid>
-
-        <Grid col={12}>
-        <ScrollableAnchor id={'result'}>
-          <Result />
-        </ScrollableAnchor>
-        </Grid>
-
-      </div>
+      </Grid>
     )
   }
 }
