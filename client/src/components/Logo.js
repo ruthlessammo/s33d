a80c816c76bb05b2b47ce4bf860fc5de9ec4551c
William Hilton <williamjchilton@gmail.com>
2017-09-19 17:36:27 +0100

    WIP

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '../css/App.css'
 
 class Logo extends Component {
   render() {
     return (
       <div>
-        <h1 className="logo">S<span>&#9776;&#9776;D</h1>
+        <h1 className="logo">S<span>&#9776;&#9776;D</span></h1>
         <p><em>Sowing Energetic Educational Development</em></p>
       </div>
     )
