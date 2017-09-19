0b074ac29be47a9f0751b406c4bbe5f6522ab167
William Hilton <williamjchilton@gmail.com>
2017-09-19 20:27:59 +0100

    wippp

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import '../css/App.css'
+import '../css/App.css';
 
 class Logo extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.jumble = this.jumble.bind(this);
+  }
+
+  jumble() {
+    let word;
+    let words = ['Evolutional', 'Energetic', 'Educational'];
+    for (let i=0; i<words.length; i++) {
+      word = words;
+      console.log(this.word);
+      return word;
+    }
+
+  
+  }
   render() {
     return (
       <div>
-        <h1 className="logo">S<span>&#9776;&#9776;D</span></h1>
-        <p><em>Sowing Energetic Educational Development</em></p>
+        <h1 className="logo">S<span className="logoSpan">&#9776;&#9776;D</span></h1>
+        <p><em>Sowing {this.word} Development</em></p>
       </div>
     )
   }
