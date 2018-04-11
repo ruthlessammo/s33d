b8805d071afe270f67469541d9a213b70ab73ac8
ruthlessammo <williamjchilton@gmail.com>
2018-04-11 14:12:38 +0100

    deleted old files

--- config/environment.js
+++
@@ -1,5 +0,0 @@
-const env = process.env.NODE_ENV || 'development';
-const port = process.env.PORT || 4001;
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/s33d-database-${env}`;
-
-module.exports = { port, env, dbURI };
